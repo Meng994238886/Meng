f572401f6259a022bc9c08815dea7b993f2e7402
Meng994238886 <994238886@qq.com>
2019-03-20 08:48:39 +0800

    3.20日考

---
+++ rk.php
@@ -0,0 +1,39 @@
+<?php 
+
+echo ugliy(6);
+
+function ugliy($a){
+
+
+
+
+
+for ($i=0; $i < $a; $i++) { 
+	# code...
+	if($a/2==0){
+		echo 1;
+	}else{
+		echo 2;
+	}
+}
+
+for ($i=0; $i < $a; $i++) { 
+	# code...
+	if($a/3==0){
+		echo 1;
+	}else{
+		echo 2;
+	}
+}
+
+for ($i=0; $i < $a; $i++) { 
+	# code...
+	if($a/5==0){
+		echo 1;
+	}else{
+		echo 2;
+	}
+}
+}
+
+?>
